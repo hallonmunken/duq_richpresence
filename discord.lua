f86a106bc8b1bd30a3bc56653fb7418847a95bb7
duqaqon <liam.ghaziri@gmail.com>
2020-09-21 20:56:41 +0200

    ✨ Updated main file

--- discord.lua
+++ discord.lua
@@ -4,14 +4,18 @@ Citizen.CreateThread(function()
         local player = GetPlayerPed(-1)
         Citizen.Wait(5*1000)
 
-        SetDiscordAppId(0)
-        SetRichPresece()
+        SetDiscordAppId(0) -- Replace 0 with your application client id.
+
+        -- Where the player is located
+        SetRichPresece( GetPlayerName(source).. "is located on".. GetStreetNameFromHashKey( table_unpack ( GetEntityCoords(player) )) )
 
 
-        SetDiscordRichPresenceAsset("hi big")
+        SetDiscordRichPresenceAsset("hi big") -- The name of the big picture you added in the application.
         SetDiscordRichPresenceAssetText(GetPlayerName(source))
 
-        SetDiscordRichPresenceAssetSmall("hi small")
+        SetDiscordRichPresenceAssetSmall("hi small") -- The name of the small picture you added in the application.
+        SetDiscordRichPresenceAssetSmallText("Health: ".. (GetEntityHealth(player) - 100 ) )
         
+    end
 
-)
+end)
