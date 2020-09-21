797179b784903778ad1766c19947435bcd9f777c
duqaqon <liam.ghaziri@gmail.com>
2020-09-21 19:52:53 +0200

    ✨ Resource file

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,3 +1,6 @@
+-- PUT THIS FILE IN YOUR SERVER RESOURCE FOLDER
+
+
 resource_manifest_version '0' --Replace 0 with your manifest version of your server, located in your resource folder at fxmanifest.lua.
 
 client_script "discord.lua" -- Where the code of the RPC is located
